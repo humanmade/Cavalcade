756e4cef690eaed50f5a7ce0c3f0b44067db0533
Joe Hoyle <joehoyle@gmail.com>
2015-07-05 21:15:58 +0200

    CLI command for showing completed jobs

--- class-command.php
+++ class-command.php
@@ -31,8 +31,48 @@ class Command extends WP_CLI_Command {
 		 * @param array  $args The arguments to be passed to the hook.
 		 */
 		do_action_ref_array( $job->hook, $job->args );
+	}
 
-		// Assume it succeeded if we got this far :)
-		$job->mark_completed();
+	/**
+	 * Show logs on completed jobs
+	 *
+	 * @synopsis [--format=<format>] [--fields=<fields>] [--job=<job-id>] [--hook=<hook>]
+	 */
+	public function log( $args, $assoc_args  ) {
+
+		global $wpdb;
+
+		$log_table = $wpdb->prefix . 'cavalcade_logs';
+		$job_table = $wpdb->prefix . 'cavalcade_jobs';
+
+		$assoc_args = wp_parse_args( $assoc_args, array(
+			'format'  => 'table',
+			'fields'  => 'job,hook,timestamp,status',
+			'hook'    => null,
+			'job'     => null,
+		));
+
+		$where = '1 = 1';
+		$data  = array();
+
+		if ( $assoc_args['job'] ) {
+			$where .= " AND job = %d";
+			$data[] = $assoc_args['job'];
+		}
+
+		if ( $assoc_args['hook'] ) {
+			$where .= " AND hook = %s";
+			$data[] = $assoc_args['hook'];
+		}
+
+		$query = "SELECT $log_table.*, $job_table.hook FROM {$wpdb->prefix}cavalcade_logs INNER JOIN $job_table ON $log_table.job = $job_table.id WHERE $where";
+
+		if ( $data ) {
+			$query = $wpdb->prepare( $query, $data );
+		}
+
+		$logs = $wpdb->get_results( $query );
+
+		\WP_CLI\Utils\format_items( $assoc_args['format'], $logs, explode( ',', $assoc_args['fields'] ) );
 	}
 }
