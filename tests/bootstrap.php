72f36c4bdfc522c775ce00033f6c12bcbe9edfb8
Ryan McCue <me@ryanmccue.info>
2017-06-30 15:36:43 +1000

    Correct coding standards

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,6 @@
  * Bootstrap the plugin unit testing environment.
  *
  * @package WordPress
- * @subpackage JSON API
 */
 
 // Support for:
@@ -12,17 +11,16 @@
 // 3. Tests checked out to /tmp
 if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
 	$test_root = getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit';
-} else if ( file_exists( '../../../../tests/phpunit/includes/bootstrap.php' ) ) {
+} elseif ( file_exists( '../../../../tests/phpunit/includes/bootstrap.php' ) ) {
 	$test_root = '../../../../tests/phpunit';
-} else if ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
+} elseif ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
 	$test_root = '/tmp/wordpress-tests-lib';
 }
 
 require $test_root . '/includes/functions.php';
 
-function _manually_load_plugin() {
+tests_add_filter( 'muplugins_loaded', function () {
 	require dirname( __DIR__ ) . '/plugin.php';
-}
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+});
 
 require $test_root . '/includes/bootstrap.php';
