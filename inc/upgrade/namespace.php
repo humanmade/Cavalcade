538c7102cffba7a616cdaa3491b5a38aeefd866a
Peter Wilson <git-commit-by@peterwilson.cc>
2018-08-23 10:49:27 +1000

    #29: An upgrade routine for bumping database verions.

---
+++ inc/upgrade/namespace.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace HM\Cavalcade\Plugin\Upgrade;
+
+use const HM\Cavalcade\Plugin\DATABASE_VERSION;
+
+/**
+ * Update the Cavalcade database version if required.
+ *
+ * Checks the Cavalcade database version and runs the
+ * upgrade routines as required.
+ *
+ * @return bool False if upgrade not required, true if run.
+ */
+function upgrade_database() {
+	$database_version = (int) get_site_option( 'cavalcade_db_version' );
+
+	if ( $database_version === DATABASE_VERSION ) {
+		// No upgrade required.
+		return false;
+	}
+
+	if ( $database_version < 2 ) {
+		upgrade_database_2();
+	}
+
+	update_site_option( 'cavalcade_db_version', DATABASE_VERSION );
+
+	// Upgrade successful.
+	return true;
+}
+
+/**
+ * Upgrade Cavalcade database tables to version 2.
+ *
+ * Add the `schedule` column to the jobs table.
+ */
+function upgrade_database_2() {
+	global $wpdb;
+
+	$query = "ALTER TABLE `{$wpdb->base_prefix}cavalcade_jobs`
+			  ADD `schedule` varchar(255) DEFAULT NULL
+			  AFTER `interval`";
+
+	$wpdb->query( $query );
+}
