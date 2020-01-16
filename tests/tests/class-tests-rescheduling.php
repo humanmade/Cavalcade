dd524956d076ca2c838704c21161fdd1bb2a58f9
Robert O'Rourke <robert@o-rourke.org>
2020-01-16 15:35:49 +0000

    renamed rescheduling tests class

---
+++ tests/tests/class-tests-rescheduling.php
@@ -0,0 +1,37 @@
+<?php
+namespace HM\Cavalcade\Tests;
+
+use WP_UnitTestCase;
+
+/**
+ * Test rescheduling an event is successful.
+ *
+ * @ticket 64
+ */
+class Tests_Rescheduling extends WP_UnitTestCase {
+	function setUp() {
+		parent::setUp();
+		// make sure the schedule is clear
+		_set_cron_array( [] );
+	}
+
+	function tearDown() {
+		// make sure the schedule is clear
+		_set_cron_array( [] );
+		parent::tearDown();
+	}
+
+	function test_recheduling() {
+		$timestamp = time() + HOUR_IN_SECONDS;
+		$key = md5( serialize( [] ) );
+
+		wp_schedule_event( $timestamp, 'hourly', 'cavalcade_repeat' );
+		wp_schedule_event( $timestamp, 'daily', 'cavalcade_repeat' );
+
+		$next_scheduled = _get_cron_array()[ wp_next_scheduled( 'cavalcade_repeat' ) ];
+		$expected = wp_get_schedules()['daily']['interval'];
+		$actual = $next_scheduled['cavalcade_repeat'][ $key ]['interval'];
+
+		$this->assertEquals( $expected, $actual );
+	}
+}
